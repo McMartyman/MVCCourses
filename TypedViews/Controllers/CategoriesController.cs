a61f95700965e9cd42870facf31b4151ecf210d1
McMartyman <tmacharaga25@gmail.com>
2024-03-20 12:52:30 +0200

    Add project files.

---
+++ TypedViews/Controllers/CategoriesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TypedViews.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Edit(int ? id)
+        {
+            //return View();
+            if (id.HasValue) { 
+                return new ContentResult { Content = id.ToString() };
+            }
+            else {
+                return new ContentResult { Content = "Not found content" };
+            }
+        }
+    }
+}
